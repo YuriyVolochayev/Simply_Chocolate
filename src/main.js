6a7c692ba43d033155760a66086c6fda429f6515
volochayev <stingray8888831@gmail.com>
2025-07-11 09:31:38 +0300

    add hero markup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+// const Arr = [1, 2, 3, 5, 6, 10]
 
+// function squareArr(newArr) {
+//     for (let index = 0; index < newArr.length; index++) {
+//         newArr[index] *= newArr[index]
+//     }
+//     return newArr;
+    
+// }
+// console.log(squareArr(Arr));
