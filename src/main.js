91c0759d21c41c0b6d1c7f9401a8385487b668b5
volochayev <stingray8888831@gmail.com>
2025-07-14 19:58:00 +0300

    add benefits

--- src/main.js
+++ src/main.js
@@ -22,3 +22,22 @@
 
 // const bmi = calcBmi("88,3", "1.75");
 // console.log(bmi);
+
+
+// const sum = 499;
+
+// if(sum >= 1500) {
+//     console.log("level 1");
+// } else if(sum > 500) {
+//     console.log("level 2");
+// } else if(sum === 500) {
+//     console.log("level 3");
+// } else if(sum < 500) {
+//     console.log("level 4");
+//     }
+
+// const sum = 1000;
+
+// const message = sum >= 0 ? 'Positive' : 'Negative';
+
+// console.log(message);
