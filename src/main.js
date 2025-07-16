1fb236a5090c04e2cbb6814b5089d97933d4cd7f
volochayev <stingray8888831@gmail.com>
2025-07-17 00:23:21 +0300

    add ingr

--- src/main.js
+++ src/main.js
@@ -55,3 +55,99 @@
     
 // console.log(time);
 
+// const deadline = 4;
+// let msg;
+
+// if(deadline === 0) {
+//     msg = `Today`
+// } else if(deadline === 1) {
+//     msg = `Tomorrow`
+// } else if(deadline === 2) {
+//     msg = `Overmorrow`
+// } else if (deadline >= 3) {
+//     msg = `Date in the future`
+// }
+
+// console.log(msg);
+
+// const user = "Petya";
+// switch (user) {
+//     case "Alice":
+//         console.log("case1");
+//         break;
+//     case "Petya":
+//         console.log("case2");
+//         break;
+//     default:
+//         console.log("Default");
+        
+//         break;
+// }
+
+// const deadline = 2;
+// let msg;
+
+// switch (deadline) {
+//     case 0:
+//         msg = 'Today';
+        
+//         break;
+//     case 1:
+//         msg = 'Tomorrow';
+        
+//         break;
+//     case 2:
+//         msg = 'Overmorrow';
+        
+//         break;
+
+//     default:
+//         msg = 'Date in the future'
+//         break;
+// }
+
+// console.log(msg);
+
+// const option = 4;
+
+// let msg;
+
+// switch (option) {
+//     case 1:
+//         msg = 'Самовивіз';
+//         break;
+    
+//     case 2:
+//         msg = 'Курьер';
+//         break;
+    
+//     case 3:
+//         msg = 'Почтомат';
+//         break;
+
+//     default:
+//         msg = "Ви не вибрали спосіб доставки"
+//         break;
+// }
+// console.log(msg);
+
+// const sub = "Pro";
+
+// let canConnect = sub === "Vip" || sub === "Pro" ? true : false
+
+// // if (sub === "Pro" || sub === "Vip") {
+// //     canConnect = true
+// // } else {
+// //     canConnect = false
+// // };
+
+// console.log("canConnect", canConnect);
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+
+// console.log("canOpenChat", canOpenChat);
+
