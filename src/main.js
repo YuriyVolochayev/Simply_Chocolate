e1d34cdcf7c984243dc5f5c4436b384a5959a272
volochayev <stingray8888831@gmail.com>
2025-07-28 15:44:41 +0300

    add some

--- src/main.js
+++ src/main.js
@@ -1510,6 +1510,94 @@
 
 //======================================REDUCE=============================================
 
+//перебирає оригінальний масив поелементно
+// значення, що повертає залежить від розробника
+
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 25, 90];
+
+// const total = num.reduce((acc, item, index, arr) => {//acc - акомулятор в який ми зберігаємо проміжні дані
+//     return acc + item;
+// }, 0)
+
+
+// console.log(total);
+
+// const salary = {
+//     mango: 1200,
+//     poly: 2000,
+//     ajax: 1500,
+// }
+
+// const total = Object.values(salary).reduce((acc, item) => acc + item);
+
+
+// console.log(total);
+
+
+//РОЗРАХУВАТИ ЗАГАЛЬНУ КІЛЬКІСТЬ ГОДИН
+
+// const players = [
+//     { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: false },
+//     { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: true },
+//     { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
+//     { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
+//     { id: "player-5", name: "Chelsy", timePlayed: 110, points: 48, online: true },
+// ];
+
+// const hours = players.reduce((acc, player) => acc + player.timePlayed, 0);
+
+
+// console.log(hours);
+
+
+
+//=====================================toSorted=========================================
+//не змінює вихідний масив а поевртає новий
+
+// const arr = [1, 22, 2, 5, 33, 7, 8, 45, 9];
+
+// const sorted = arr.toSorted()
+
+// console.log(sorted);
+
+// const letters = ["B", "a", "b", "A"];
+
+// const sorted = letters.toSorted((a, b) => a.localeCompare(b));
+// console.log(sorted);
+
+
+//=====================compareFunction - функція порівняння====================================
+
+// const arr = [1, 22, 2, 5, 33, 7, 8, 45, 9];
+
+// const res1 = arr.toSorted((a, b) => a - b);
+// const res2 = arr.toSorted((a, b) => b - a);
+// console.log(res1);
+// console.log(res2);
+
+
+
+//===============================ЛАНЦЮЖКИ МЕТОДІВ===================================
+
+// const num = [1, 22, 2, 5, 33, 7, 8, 45, 9];
+
+// const arr = num.filter((num) => num > 5);
+
+// const arr2 = arr1.map((num) => num * 2);
+
+// const arr3 = arr2.toSorted((x, y) => x - y);
+
+// використаємо ланцюжок методів
+
+// const newArray = num
+//     .filter((num) => num > 5)
+//     .map((num) => num * 2)
+//     .toSorted((x, y) => x - y)
+
+
+// console.log(newArray);
+// console.log(arr2);
+// console.log(arr3);
 
 
 
